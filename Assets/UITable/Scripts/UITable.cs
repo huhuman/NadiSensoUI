2d0c90c3b996e82449bf4a6d7bfda3868769afa5
Zilong <jim8120004@gmail.com>
2019-05-11 20:27:37 +0800

    上傳table UI

---
+++ Assets/UITable/Scripts/UITable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UIPlugin.Table {
+    public class UITable : MonoBehaviour {
+        [Header ("欄位數上限")]
+        public int maxTableRow;
+        // 暫存當前產生的欄位數
+        private int nowTableRow { get { return uiItems.Count; } }
+
+        [Header ("表格欄位Prefab")]
+        public GameObject uITableItem;
+        public Transform uITableItemParent;
+        public List<UITableItem> uiItems = new List<UITableItem> ();
+
+        public void CreateTable (string[] data, bool isTitle = false) {
+            if (nowTableRow >= maxTableRow) { return; }
+            UITableItem uiItem = GameObject.Instantiate (uITableItem, uITableItemParent).GetComponent<UITableItem> ();
+            uiItem.SetDataToTable (data, isTitle);
+            uiItems.Add (uiItem);
+        }
+    }
+}
---
+++ Assets/UITable/Scripts/UITableItem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UIPlugin.Table {
+    public class UITableItem : MonoBehaviour {
+        [Header ("第一列資料")]
+        public Text tableCol1;
+        [Header ("第二列資料")]
+        public Text tableCol2;
+        [Header ("底圖")]
+        public Image tableBg;
+        /**
+         * 設置資料
+         */
+        public void SetDataToTable (string[] data, bool isTitle = false) {
+            tableCol1.text = data[0];
+            tableCol2.text = data[1];
+            // 如果為標題的話底色變色
+            if (isTitle) {
+                tableBg.color = Color.grey;
+            }
+        }
+    }
+}
