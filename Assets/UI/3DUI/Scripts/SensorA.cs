f6b6b31a5927ae5f2942b0146dc52d257719acac
Zilong <>
2019-05-14 14:19:31 +0800

    更新sensorA

---
+++ Assets/UI/3DUI/Scripts/SensorA.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SensorUI {
+    public class SensorA : SensorBase {
+        // Update is called once per frame
+        void Update () { }
+        public override void ShowUI () {
+            sensorUI.gameObject.SetActive (true);
+            string[] inputData = new string[3] { "點膠機稼動率", "19", "-1" }; // name, staus, var
+            sensorUI.Show (inputData);
+            DrawLine ();
+        }
+
+        public override void CloseUI () {
+            sensorUI.Close ();
+        }
+    }
+}
--- Assets/UI/Base/Scripts/SensorBase.cs
+++ Assets/UI/Base/Scripts/SensorBase.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 namespace SensorUI {
+    [RequireComponent (typeof (LineRenderer))]
     public class SensorBase : MonoBehaviour {
         [Header ("Sensor 名稱")]
         public string sensorType;
@@ -9,9 +10,12 @@ namespace SensorUI {
         public SensorUIBase sensorUI;
         [Header ("資料ID")]
         public int dataId;
+        protected LineRenderer lineRenderer;
+
         // sensor把自己透過sensor類型註冊到管理器內
         protected virtual void Awake () {
             SensorManagerBase.Instance.AddSensor (sensorType, this);
+            lineRenderer = this.GetComponent<LineRenderer> ();
         }
 
         // 複寫顯示 UI 時該怎麼拿資料及處理資料並顯示UI與把資料指定給UI，可能會有的額外功能是顯示動畫
@@ -23,6 +27,10 @@ namespace SensorUI {
         // 複寫關閉 UI ，可能會有的額外功能是關閉動畫
         public virtual void CloseUI () {
             // TODO
+        }
+        protected virtual void DrawLine () {
+            lineRenderer.SetPosition (0, this.transform.position);
+            lineRenderer.SetPosition (1, sensorUI.transform.position);
         }
     }
 }
